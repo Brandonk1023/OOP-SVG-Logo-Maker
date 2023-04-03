7c6f1d450a64732fe050262b08fad24ca35cf971
Brandon Kilgore <brandonk1023@gmail.com>
2023-04-03 10:17:26 -0400

    added questions for inquirer for logo creation

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const shapes = require('lib/shapes.js')
+
+const questions = [
+    {
+        type: 'checkbox',
+        name: 'shape',
+        message: 'Please select shape.',
+        choices: ['circle', 'square', 'triangle'],
+    },
+
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter text needed for logo, of no text please leave blank.'
+    },
+]
