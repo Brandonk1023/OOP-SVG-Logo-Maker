c70541abeb445ffce35d3b5fc4c17215feb2d40d
Brandon Kilgore <brandonk1023@gmail.com>
2023-04-03 13:34:52 -0400

    added more questions for logo generation and changed checkbox to list type

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const shapes = require('lib/shapes.js')
 
 const questions = [
     {
-        type: 'checkbox',
+        type: 'list',
         name: 'shape',
         message: 'Please select shape.',
         choices: ['circle', 'square', 'triangle'],
@@ -12,7 +12,19 @@ const questions = [
 
     {
         type: 'input',
+        name: 'shapeColor',
+        message: 'Please enter in the fill color name. Hex code can be entered in place of color name.',
+    },
+
+    {
+        type: 'input',
         name: 'text',
         message: 'Please enter text needed for logo, of no text please leave blank.'
+    },
+
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Please enter in fill color name. Hex code can be entered in place of color name',
     },
 ]
